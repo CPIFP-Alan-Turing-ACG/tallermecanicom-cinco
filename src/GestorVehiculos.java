cd5c759a3c71a7e037c507218b69b90b4d1ec1c1
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2026-02-03 12:31:17 +0100

    first commit

--- src/GestorVehiculos.java
+++ src/GestorVehiculos.java
@@ -1,6 +1,12 @@
 public class GestorVehiculos {
+
     public void registrarVehiculo() {
         // TODO: Implementar lógica (Alumno A)
-        System.out.println("Vehiculos registrados: 4");
+        System.out.println("Vehiculos registrados: 4 ");
     }
+    
+    public void comprobarStock() {
+        System.out.println("Stock actual: 50 Litros de Aceite, 4 Neumáticos.");
+    }
+       
 }
