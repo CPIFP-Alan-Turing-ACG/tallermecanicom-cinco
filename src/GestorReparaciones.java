79ca2580ae14475cbd927fb0d6e1b2679278c203
Yeray Cortés <jcorbue2706@g.educaand.es>
2026-02-03 12:50:09 +0100

    first commit

--- src/GestorReparaciones.java
+++ src/GestorReparaciones.java
@@ -2,5 +2,9 @@ public class GestorReparaciones {
     public void procesarReparacion() {
         // TODO: Implementar lógica (Alumno B)
         System.out.println("Función no implementada aún");
+        // En la zona del TODO del Alumno B escribe:
+        GestorReparaciones reparaciones = new GestorReparaciones();
+        reparaciones.procesarReparacion();
+
     }
 }
